da40b13e9eba1e6211fc8d85882a8811b6332986
Lyudmila Ostroumova <lyudmila.ostroumova@gmail.com>
2021-09-15 22:03:10 +0300

    scr added

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int ticketPrice;
+        int miles;
+        int result;
+        ticketPrice = 2000052;
+        miles = ticketPrice / 20;
+        result = miles;
+        System.out.println("Накоплено миль:" + result);
+
+    }
+}
